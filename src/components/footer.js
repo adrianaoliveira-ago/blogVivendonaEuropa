f45ff9dde35a1213d38fd752f6af8d886b754a18
Adriana Gonzaga <adrianagonzaga@Adrianas-iMac.fritz.box>
2023-10-04 20:34:10 +0200

    added a footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,10 +2,21 @@ import * as React from "react"
 
 import "./footer.css"
 
+import IconYoutube from "./iconYoutube.svg"
+import IconInstagram from "./iconInstagram.svg"
+import IconFacebook from "./iconFacebook.svg"
+
 const FooterWave = () => {
   return (
     <footer>
-      <div className="footer-container-wave" />
+      <div className="footer-conatainer">
+        <div className="footer-container-wave"></div>
+        <div>
+          <img src={IconYoutube}></img>
+          <img src={IconInstagram}></img>
+          <img src={IconFacebook}></img>
+        </div>
+      </div>
     </footer>
   )
 }
